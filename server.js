b7d18624b9e123023649b5381363d638f76643a3
lukereynoldspi <lukereynoldspi@gmail.com>
2023-04-22 16:19:12 -0700

    added testing packages, updated get/post/put requests

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const shortid = require('shortid');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 3003
 
-let businesses = [];
+let businesses = [
+  {name:"Luke Inc.", 
+  address:"Bruh Rd.", 
+  city:"Bend", 
+  state:"OR", 
+  zipCode:"12345", 
+  phoneNumber:"(541) 555-5555", 
+  category:"Technology", 
+  subcategory:"Software"}
+];
 
 app.use(cors());
 
@@ -12,21 +22,56 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/business', (req, res) => {
-  const name = req.body.name;
-  const address = req.body.address;
-  const city = req.body.city;
-  const state = req.body.state;
-  const zipCode = req.body.zipCode;
-  const phoneNumber = req.body.phoneNumber;
-  const category = req.body.category;
-  const subcategory = req.body.subcategory;
-  console.log(business);
-    businesses.push(business);
+// Add a business
+app.post('/businesses', (req, res) => {
+  const business = {
+    id: shortid.generate(),
+    name: req.body.name,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    zipCode: req.body.zipCode,
+    phoneNumber: req.body.phoneNumber,
+    category: req.body.category,
+    subcategory: req.body.subcategory
+  };
+  
+  businesses.push(business);
 
-    res.send('Business is added to the database');
+  console.log(businesses);
+  res.send('Business is added to the database');
+});
+
+// Update a business
+app.put('/businesses/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const business = businesses.find(business => business.id === id);
+  if (business) {
+    business.name = req.body.name;
+    business.address = req.body.address,
+    business.city = req.body.city,
+    business.state = req.body.state,
+    business.zipCode = req.body.zipCode,
+    business.phoneNumber = req.body.phoneNumber,
+    business.category = req.body.category,
+    business.subcategory = req.body.subcategory
+    res.json(business);
+  } else {
+    res.status(404).send('Business not found');
+  }
+});
+
+// Get all businesses
+app.get('/businesses', (req, res) => {
+  res.json(businesses);
+  console.log('Looking at all businesses...')
+});
+
+// Welcome message
+app.get('/', (req, res) => {
+  res.send('Welcome to the Luke Reynolds Business API!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
